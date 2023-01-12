e7ab6f0d3b09c722ecb7d2271467e3a8b3953c97
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-13 00:03:35 +0800

    update css

--- 3-reviews/src/components/Review.js
+++ 3-reviews/src/components/Review.js
@@ -1,32 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/Review.css";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 export default function Review({ review }) {
+  const [index, setIndex] = useState(0);
+  const { name, role, info, image } = review[index];
   return (
     <>
-      {review.map((review) => {
-        const { id, name, role, info, image } = review;
-        return (
-          <section key={id} className="single-review">
-            <img src={image} alt={name} />
-            <div className="profile">
-              <h2>{name}</h2>
-              <h4>{role}</h4>
-              <p>{info}</p>
-            </div>
-            <div className="move-container">
-              <button>
-                <FaChevronLeft />
-              </button>
-              <button>
-                <FaChevronRight />
-              </button>
-            </div>
-            <button className="random-btn">Surprise Me</button>
-          </section>
-        );
-      })}
+      <section key={index} className="single-review">
+        <div className="image-container">
+          <img src={image} alt={name} />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <div className="profile">
+          <h2>{name}</h2>
+          <h4>{role}</h4>
+          <p>{info}</p>
+        </div>
+        <div className="move-container">
+          <button>
+            <FaChevronLeft />
+          </button>
+          <button>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn">Surprise Me</button>
+      </section>
     </>
   );
 }
