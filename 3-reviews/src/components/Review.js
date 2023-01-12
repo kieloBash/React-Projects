5193e0979d8370a1f075ba33f0181ea993966749
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-13 00:21:35 +0800

    added left and right function

--- 3-reviews/src/components/Review.js
+++ 3-reviews/src/components/Review.js
@@ -5,6 +5,34 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 export default function Review({ review }) {
   const [index, setIndex] = useState(0);
   const { name, role, info, image } = review[index];
+
+  const checkNumber = (temp) => {
+    if (temp > review.length - 1) {
+      return 0;
+    }
+    if (temp < 0) {
+      return review.length - 1;
+    }
+    return temp;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      return checkNumber(index+1);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      return checkNumber(index-1);
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * review.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <>
       <section key={index} className="single-review">
@@ -20,14 +48,16 @@ export default function Review({ review }) {
           <p>{info}</p>
         </div>
         <div className="move-container">
-          <button>
+          <button onClick={prevPerson}>
             <FaChevronLeft />
           </button>
-          <button>
+          <button onClick={nextPerson}>
             <FaChevronRight />
           </button>
         </div>
-        <button className="random-btn">Surprise Me</button>
+        <button className="random-btn" onClick={randomPerson}>
+          Surprise Me
+        </button>
       </section>
     </>
   );
