ea17ff9ed5d86a562a354de9a5a46133fa177559
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-12 23:52:11 +0800

    added react icons fa

--- 3-reviews/src/components/Review.js
+++ 3-reviews/src/components/Review.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./css/Review.css";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export default function Review({ review }) {
   return (
@@ -7,16 +8,20 @@ export default function Review({ review }) {
       {review.map((review) => {
         const { id, name, role, info, image } = review;
         return (
-          <section key={id} className='single-review'>
+          <section key={id} className="single-review">
             <img src={image} alt={name} />
             <div className="profile">
-                <h2>{name}</h2>
-                <h4>{role}</h4>
-                <p>{info}</p>
+              <h2>{name}</h2>
+              <h4>{role}</h4>
+              <p>{info}</p>
             </div>
             <div className="move-container">
-                <button>left</button>
-                <button>right</button>
+              <button>
+                <FaChevronLeft />
+              </button>
+              <button>
+                <FaChevronRight />
+              </button>
             </div>
             <button className="random-btn">Surprise Me</button>
           </section>
