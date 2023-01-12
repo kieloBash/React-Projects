6ed6125a563ba347769e04f0af64e970caf50425
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-12 19:18:52 +0800

    added loading screen then fetch

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Loading from "./components/Loading";
-import Tour from "./components/Tour";
 import Tours from "./components/Tours";
+const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+  }
+useEffect(() => {
+  fetchTours();
+}, []);
   if (loading) {
     return (
       <main>
