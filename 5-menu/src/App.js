f52cb83cc76301a84d3eada851a3ba6b2f67a159
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-13 12:54:19 +0800

    applied css 5-menu

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
+import items from './Data';
+import Categories from './components/Categories';
+import Menu from './components/Menu';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories,setCategories] = useState([]);
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
   );
 }
 
--- 5-menu/src/components/Menu.js
+++ 5-menu/src/components/Menu.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-export default function Menu() {
-    return (
-        <h2>Menu</h2>
-    );
+export default function Menu(props) {
+  return (
+    <div className="section-center">
+      {props.items.map((item) => {
+        const { id, title, img, desc, price } = item;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">{price}</h4>
+              </header>
+              <hr></hr>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
