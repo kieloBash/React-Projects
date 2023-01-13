fc0204ad8df610b872f7036089393d79efeacf46
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-13 13:20:12 +0800

    dynamic approach done

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
-import React, { useState } from 'react';
-import items from './Data';
-import Categories from './components/Categories';
-import Menu from './components/Menu';
+import "./App.css";
+import React, { useState } from "react";
+import items from "./Data";
+import Categories from "./components/Categories";
+import Menu from "./components/Menu";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories,setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
-  function filterItems(category){
-    if(category === 'all'){
+  function filterItems(category) {
+    if (category === "all") {
       setMenuItems(items);
       return;
     }
-      const newItems = items.filter((item)=> item.category === category)
-      setMenuItems(newItems);
-  };
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
 
   return (
     <main>
-      <section className='menu section'>
-        <div className='title'>
+      <section className="menu section">
+        <div className="title">
           <h2>Our Menu</h2>
-          <div className='underline'></div>
+          <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems}/>
-        <Menu items={menuItems}/>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
--- 5-menu/src/components/Categories.js
+++ 5-menu/src/components/Categories.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export default function Categories({filterItems}){
-    return (
-        <div className='btn-container'>
-            <button onClick={()=>filterItems('all')}>All</button>
-            <button onClick={()=>filterItems('breakfast')}>Breakfast</button>
-            <button onClick={()=>filterItems('lunch')}>Lunch</button>
-            <button onClick={()=>filterItems('shakes')}>Shakes</button>
-        </div>
-    );
+export default function Categories({ categories, filterItems }) {
+  return (
+    <div className="btn-container">
+      {/* <button onClick={() => filterItems("all")}>All</button>
+      <button onClick={() => filterItems("breakfast")}>Breakfast</button>
+      <button onClick={() => filterItems("lunch")}>Lunch</button>
+      <button onClick={() => filterItems("shakes")}>Shakes</button> */}
+
+      {categories.map((category, index) => {
+        return (
+          <button key={index} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
