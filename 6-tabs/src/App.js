b2b14e8d614f00ca108642e9a5ae439001a5e7f1
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-13 18:02:22 +0800

    done css with tabs

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
@@ -25,12 +26,51 @@ function App() {
   }
   const { company, dates, duties, title } = jobs[value];
   return (
-  <section className="main">
-    <div className="title">
-      <h1>Experience</h1>
-      <div className="underline"></div>
-    </div>
-  </section>
+    <section className="main">
+      <div className="title">
+        <h1>Experience</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="job-container">
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.id}
+                onClick={() => {
+                  setValue(index);
+                }}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {job.company}
+              </button>
+            );
+          })}
+        </div>
+        <div className="main-jobs">
+          <header>
+            <h1 className="title-text">{title}</h1>
+            <h3 className="company">{company}</h3>
+            <h3 className="dates">{dates}</h3>
+          </header>
+          <div className="duties-container">
+            {duties.map((duty, index) => {
+              return (
+                <div className="duty" key={index}>
+                  <span className="bullet">
+                    <FaAngleDoubleRight />
+                  </span>
+                  {duty}
+                </div>
+              );
+            })}
+          </div>
+          <div className="info-container">
+            <button className="btn-info">More Info</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
